03a8931149c884283e5c321581b0cf7b287dd5d8
Brian Marshall <bmarshall@RIIS.com>
2017-02-07 16:35:14 -0500

    implemented MVVM for CompanyDetailsActivity

--- app/src/main/java/riis/etadetroit/view/CompanyDetailsActivity.java
+++ app/src/main/java/riis/etadetroit/view/CompanyDetailsActivity.java
@@ -1,103 +1,43 @@
 package riis.etadetroit.view;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.databinding.DataBindingUtil;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.graphics.Palette;
 import android.transition.Transition;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import riis.etadetroit.R;
-import riis.etadetroit.adapters.RouteCursorAdapter;
 import riis.etadetroit.adapters.TransitionAdapter;
-import riis.etadetroit.model.CompanyData;
-import riis.etadetroit.model.ETADetroitDatabaseHelper;
+import riis.etadetroit.databinding.ActivityCompanyDetailsBinding;
+import riis.etadetroit.viewmodel.CompanyDetailsViewModel;
 
 public class CompanyDetailsActivity extends Activity {
 
     public static final String EXTRA_PARAM_ID = "place_id";
-    private ListView mList;
-    private ImageView mImageView;
-    private TextView mTitle;
-    private LinearLayout mTitleHolder;
-
-    private Cursor routeCursor;
-    private int defaultColor;
-    private String companyName;
-    private int companyImageResourceId;
-
-    private ETADetroitDatabaseHelper eTADetroitDatabaseHelper;
+    private CompanyDetailsViewModel companyDetailsViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_company_details);
 
-        eTADetroitDatabaseHelper = new ETADetroitDatabaseHelper(this);
+        ActivityCompanyDetailsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_company_details);
 
-        int companyPosition = getIntent().getIntExtra(EXTRA_PARAM_ID, 0);
+        companyDetailsViewModel = new CompanyDetailsViewModel(this, getIntent().getIntExtra(EXTRA_PARAM_ID, 0));
 
-        companyName = getCompanyName(companyPosition);
-        companyImageResourceId = getCompanyImageResourceId(this, companyPosition);
+        binding.setCompanyDetailsViewModel(companyDetailsViewModel);
 
-        mList = (ListView) findViewById(R.id.list);
-        mImageView = (ImageView) findViewById(R.id.busImage);
-        mTitle = (TextView) findViewById(R.id.textView);
-        mTitleHolder = (LinearLayout) findViewById(R.id.busNameHolder);
-        defaultColor = getResources().getColor(R.color.primary_dark);
+        binding.list.setAdapter(companyDetailsViewModel.getRouteCursorAdapter());
 
+        binding.list.setOnItemClickListener(companyDetailsViewModel.getOnItemClickListener());
 
-        setUpAdapter();
-        loadBusCompany();
+        applyPalette(companyDetailsViewModel.getPalette());
+
         windowTransition();
-        getPhoto();
-
-        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                if (routeCursor.moveToPosition(position)) {
-                    Intent intent = new Intent(CompanyDetailsActivity.this, RouteDetailsActivity.class);
-                    intent.putExtra(RouteDetailsActivity.EXTRA_ROUTE, routeCursor.getString(1));
-                    startActivity(intent);
-                }
-            }
-        });
     }
 
-    public int getCompanyImageResourceId(Context context, int position) {
-        CompanyData companyData = new CompanyData(eTADetroitDatabaseHelper.getCompanyNames());
-        return companyData.getCompanyImageResourceId(context, position);
-    }
-
-    public String getCompanyName(int position) {
-        CompanyData companyData = new CompanyData(eTADetroitDatabaseHelper.getCompanyNames());
-        return companyData.getCompanyName(position);
-    }
-
-    public Cursor getRoutes(String company) {
-        return eTADetroitDatabaseHelper.getRoutes(company);
-    }
-
-    private void setUpAdapter() {
-        routeCursor = getRoutes(companyName);
-        RouteCursorAdapter routeAdapter = new RouteCursorAdapter(this, routeCursor);
-        mList.setAdapter(routeAdapter);
-    }
-
-    private void loadBusCompany() {
-        mTitle.setText(companyName);
-        mImageView.setImageResource(companyImageResourceId);
+    private void applyPalette(Palette mPalette) {
+        getWindow().setBackgroundDrawable(new ColorDrawable(mPalette.getDarkMutedColor(companyDetailsViewModel.getDefaultColor())));
     }
 
     private void windowTransition() {
@@ -109,20 +49,4 @@ public class CompanyDetailsActivity extends Activity {
         });
 
     }
-
-    private void getPhoto() {
-        Bitmap photo = BitmapFactory.decodeResource(getResources(), companyImageResourceId);
-        colorize(photo);
-    }
-
-    private void colorize(Bitmap photo) {
-        Palette mPalette = new Palette.Builder(photo).generate();
-        applyPalette(mPalette);
-    }
-
-    private void applyPalette(Palette mPalette) {
-        getWindow().setBackgroundDrawable(new ColorDrawable(mPalette.getDarkMutedColor(defaultColor)));
-        mTitleHolder.setBackgroundColor(mPalette.getMutedColor(defaultColor));
-    }
-
 }
--- app/src/main/java/riis/etadetroit/viewmodel/CompanyDetailsViewModel.java
+++ app/src/main/java/riis/etadetroit/viewmodel/CompanyDetailsViewModel.java
@@ -1,9 +1,77 @@
 package riis.etadetroit.viewmodel;
 
-/**
- * Created by bmarshall on 2/7/17.
- */
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.support.v7.graphics.Palette;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class CompanyDetailsViewModel{
+import riis.etadetroit.R;
+import riis.etadetroit.adapters.RouteCursorAdapter;
+import riis.etadetroit.model.CompanyData;
+import riis.etadetroit.model.ETADetroitDatabaseHelper;
+import riis.etadetroit.view.RouteDetailsActivity;
 
+public class CompanyDetailsViewModel {
+    private Context context;
+    private Cursor routeCursor;
+    private int defaultColor;
+    public String companyName;
+    public Drawable busImage;
+    public int titleHolderBackgroundColor;
+    private Palette mPalette;
+    private ETADetroitDatabaseHelper eTADetroitDatabaseHelper;
+
+    public CompanyDetailsViewModel(Context context, int companyPosition) {
+        this.context = context;
+
+        eTADetroitDatabaseHelper = new ETADetroitDatabaseHelper(context);
+
+        CompanyData companyData = new CompanyData(eTADetroitDatabaseHelper.getCompanyNames());
+
+        companyName = companyData.getCompanyName(companyPosition);
+
+        int companyImageResourceId = companyData.getCompanyImageResourceId(context, companyPosition);
+
+        Bitmap photo = BitmapFactory.decodeResource(context.getResources(), companyImageResourceId);
+
+        mPalette = new Palette.Builder(photo).generate();
+
+        defaultColor = context.getResources().getColor(R.color.primary_dark);
+
+        titleHolderBackgroundColor = mPalette.getMutedColor(defaultColor);
+
+        busImage = context.getResources().getDrawable(companyImageResourceId, context.getTheme());
+    }
+
+    public AdapterView.OnItemClickListener getOnItemClickListener() {
+        return new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (routeCursor.moveToPosition(position)) {
+                    Intent intent = new Intent(context, RouteDetailsActivity.class);
+                    intent.putExtra(RouteDetailsActivity.EXTRA_ROUTE, routeCursor.getString(1));
+                    context.startActivity(intent);
+                }
+            }
+        };
+    }
+
+    public RouteCursorAdapter getRouteCursorAdapter() {
+        routeCursor = eTADetroitDatabaseHelper.getRoutes(companyName);
+        return new RouteCursorAdapter(context, routeCursor);
+    }
+
+    public Palette getPalette() {
+        return mPalette;
+    }
+
+    public int getDefaultColor() {
+        return defaultColor;
+    }
 }
